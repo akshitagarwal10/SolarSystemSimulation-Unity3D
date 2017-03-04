85eb9f8215f7aebc9929f18b8fe9947549324add
Akshit Agarwal <akshitagarwal10@gmail.com>
2017-03-05 02:58:02 +0530

    SolarSystem

---
+++ Assets/Scripts/ChangeLookAtTarget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLookAtTarget : MonoBehaviour {
+
+	public GameObject target; // the target that the camera should look at
+
+	void Start() {
+		if (target == null) 
+		{
+			target = this.gameObject;
+			Debug.Log ("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
+		}
+	}
+
+	// Called when MouseDown on this gameObject
+	void OnMouseDown () {
+		// change the target of the LookAtTarget script to be this gameobject.
+		LookAtTarget.target = target;
+        if (target.transform.localScale.x > 100)
+        {
+            Camera.main.fieldOfView = 60;
+        }
+        else
+            Camera.main.fieldOfView = target.transform.localScale.x;
+	}
+}
